d1ea60703ce50ac642725eea65b86db4525a1022
Hannes Schulz <temporaer@gmx.de>
2008-10-07 00:41:30 +0200

    generate SDP problem from adjacency matrix

--- src/rand_walk_gen/sdp_rand_walk_gen.cpp
+++ src/rand_walk_gen/sdp_rand_walk_gen.cpp
@@ -1,15 +1,17 @@
 /*       Created   :  10/05/2008 10:25:05 PM
- *       Last Change: Mon Oct 06 06:00 PM 2008 CEST
+ *       Last Change: Tue Oct 07 12:00 AM 2008 CEST
  */
+#include <nana.h>
 #include <sdp_rand_walk_gen.hpp>
 #include <sdp_rand_walk_prob_gen.hpp>
+
 using namespace boost::numeric::ublas;
 using namespace std;
 RandWalkGen::RandWalkT SDPRandWalkGen::operator()(const AdjMatT& adj)
 {
-	matrix<double> omega;
+	I(adj.size1() == adj.size2());
 
-	SDPRandWalkProbGen sdp_probgen(omega,adj);
+	SDPRandWalkProbGen sdp_probgen(adj);
 
 	SDPProb prob = sdp_probgen();
 
--- src/rand_walk_gen/sdp_rand_walk_prob_gen.cpp
+++ src/rand_walk_gen/sdp_rand_walk_prob_gen.cpp
@@ -1,19 +1,55 @@
 /*       Created   :  10/06/2008 12:36:07 AM
- *       Last Change: Mon Oct 06 12:00 AM 2008 CEST
+ *       Last Change: Tue Oct 07 12:00 AM 2008 CEST
  */
 
+#include <nana.h>
 #include <sdp_rand_walk_prob_gen.hpp>
 
-SDPRandWalkProbGen::SDPRandWalkProbGen(const OmegaT&o, const AdjMatT&a)
-	: mOmega(o)
-	 ,mAdjMat(a)
+namespace ublas = boost::numeric::ublas;
+using namespace std;
+
+SDPRandWalkProbGen::SDPRandWalkProbGen(const AdjMatT&a)
+	: mAdjMat(a)
 {
 }
 
 
 SDPProb SDPRandWalkProbGen::operator()()
 {
+	using ublas::identity_matrix;
+	typedef AdjMatT Matrix ;
+
 	SDPProb prob;
+
+	I(mAdjMat.size1() == mAdjMat.size2());
+
+	int n = mAdjMat.size1();
+
+	// omega
+	Matrix omega( 2*identity_matrix<double>(n) );
+	omega(0,0)     = 1;
+	omega(n-1,n-1) = 1;
+
+	// omega^(1/2)
+	Matrix omega_1_2 ( sqrt(2) * identity_matrix<double>(n) );
+	omega_1_2(0,0)     = 1;
+	omega_1_2(n-1,n-1) = 1;
+
+	// omega^(-1/2)
+	Matrix omega_m1_2 ( (1.0/sqrt(2)) * identity_matrix<double>(n) );
+	omega_m1_2(0,0)     = 1;
+	omega_m1_2(n-1,n-1) = 1;
+
+	// C
+	noalias(prob.C) = prod(omega_1_2, Matrix(prod(mAdjMat,omega_m1_2)));
+
+	// F_0
+	identity_matrix<double> Id(n);
+	prob.F.push_back(Id);
+
+	// b_0
+	prob.b = ublas::vector<double>(1);
+	prob.b(0) = 1;
 	return prob;
 }
 
