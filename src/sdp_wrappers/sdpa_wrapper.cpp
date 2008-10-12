fd40a6abacd782dc82eacd335d57441be94cf221
Hannes Schulz <temporaer@gmx.de>
2008-10-12 16:36:08 +0200

    added sdpa-wrapper skeleton and code to detect whether it is installed

---
+++ src/sdp_wrappers/sdpa_wrapper.cpp
@@ -0,0 +1,21 @@
+/*       Created   :  10/06/2008 12:52:01 AM
+ *       Last Change: Tue Oct 07 08:00 PM 2008 CEST
+ */
+
+#include <nana.h>
+#include <csdp_wrapper.hpp>
+#include <factory/factory.h>
+CSDPWrapper::AnswerT CSDPWrapper::operator()(const SDPProb&)
+{
+	L("CSDPWrapper::operator()\n");
+	// TODO: wrap csdp.
+	AnswerT ret;
+	return ret;
+}
+ CSDPWrapper::~CSDPWrapper(){
+L("Destroying CSDPWrapper\n");
+ }
+
+namespace{
+	registerInFactory<SDPWrapper, CSDPWrapper>  registerBase("CSDPWrapper");
+}
