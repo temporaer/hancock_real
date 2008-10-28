f6c262ca98e124c7312f4cc9957b0999cc27cde9
Hannes Schulz <temporaer@gmx.de>
2008-10-28 01:18:47 +0100

    add function to write octave-style output to file

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 /*       Created   :  10/03/2008 08:22:01 PM
- *       Last Change: Mon Oct 27 11:00 PM 2008 CET
+ *       Last Change: Tue Oct 28 01:00 AM 2008 CET
  */
 
 #include <dlfcn.h>
 #include <string>
 #include <iostream>
+#include <fstream>
 #include <factory/factory.h>
 #include "configuration.hpp"
 
@@ -19,6 +20,31 @@
 using namespace boost::numeric::ublas;
 using namespace boost;
 using namespace std;
+
+template<class T,class U>
+void writeToFile(
+		const char* fn,
+		const T&    adj,
+		const U&    path
+		){
+	ofstream o(fn);
+	o << "A = [ " ;
+	for(int row=0;row<adj.size2();row++){
+		for(int col=0;col<adj.size1();col++){
+			o << adj(row,col) << " ";
+		}
+		if(row != adj.size2()-1)
+			o << " ; ";
+	}
+	o << " ];"<<endl;
+
+	o << "path = [ ";
+	for(int i=0;i<path.size();i++)
+	{
+		o << path[i].first << " ";
+	}
+	o << path.back().second << " ] ; "<< endl;
+}
 
 int main(int argc, char* argv[]){
 	dlopen("sdp_wrappers/libsdp_wrappers.so",RTLD_LAZY);
@@ -36,4 +62,7 @@ int main(int argc, char* argv[]){
 	walkgen.setSDPWrapper( sdp_wrapper );
 
 	SeriationGen::SeriationT randwalk  = walkgen(adjmat_ptr);
+
+	const char* out_file = gCfg().getString("output").c_str();
+	writeToFile(out_file,*adjmat_ptr,randwalk);
 }
