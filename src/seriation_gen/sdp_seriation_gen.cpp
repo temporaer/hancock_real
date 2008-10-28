5afe7fd2b4e5caaced90accdff7a0aa84b343bb9
Hannes Schulz <temporaer@gmx.de>
2008-10-28 01:19:26 +0100

    ported randomized hyperplane from octave

--- src/seriation_gen/sdp_seriation_gen.cpp
+++ src/seriation_gen/sdp_seriation_gen.cpp
@@ -1,5 +1,5 @@
 /*       Created   :  10/05/2008 10:25:05 PM
- *       Last Change: Mon Oct 27 07:00 PM 2008 CET
+ *       Last Change: Tue Oct 28 01:00 AM 2008 CET
  */
 #include <boost/numeric/ublas/matrix_proxy.hpp>
 #include <sdp_prob.hpp>
@@ -39,20 +39,20 @@ SeriationGen::SeriationT SDPSeriationGen::Impl::operator()(boost::shared_ptr<Adj
 	// Solve the SDP-Problem using an SDP Wrapper
 	SDPWrapper::AnswerT X = (*mSDPWrapper)(prob);
 
-	cout << "Y = [ ";
-	for(unsigned int i=0;i<X.size1();i++){
-		for(unsigned int j=0;j<X.size2();j++){
-			cout << X(i,j) << " ";
-		}
-		cout << " ; ";
-	}
-	cout << "]; \n";
+//	cout << "Y = [ ";
+//	for(unsigned int i=0;i<X.size1();i++){
+//		for(unsigned int j=0;j<X.size2();j++){
+//			cout << X(i,j) << " ";
+//		}
+//		cout << " ; ";
+//	}
+//	cout << "]; \n";
 	
+	unsigned int n = X.size1();
 	// Make sure we got the right thing: tr(EX)=1
 #ifndef NDEBUG
 	using ublas::range;
 	using ublas::prod;
-	int n = X.size1();
 	for(unsigned int i=0;i<prob.F.size();i++){
 		ublas::matrix<double> R = prod(prob.F[i],X);
 		ublas::matrix_vector_range<SDPWrapper::AnswerT> diag(R, range (0,n), range (0,n));
@@ -67,27 +67,82 @@ SeriationGen::SeriationT SDPSeriationGen::Impl::operator()(boost::shared_ptr<Adj
 	if(!ulapack::chol_checked_inplace(V, true))
 		throw runtime_error("Could not cholesky-decompose matrix, seems not to be positive-semidefinite.");
 
-	cout << "V = [ ";
-	for(unsigned int i=0;i<V.size1();i++){
-		for(unsigned int j=0;j<V.size2();j++){
-			cout << V(i,j) << " ";
-		}
-		cout << " ; ";
-	}
-	cout << "]; \n";
+//	cout << "V = [ ";
+//	for(unsigned int i=0;i<V.size1();i++){
+//		for(unsigned int j=0;j<V.size2();j++){
+//			cout << V(i,j) << " ";
+//		}
+//		cout << " ; ";
+//	}
+//	cout << "]; \n";
 
 	// random hyperplane technique
-	for(int iter=0;iter<1000;iter++){
+	ublas::vector<double> min_y;
+	float min_y_val = 1E6;
+	// TODO: intelligenter abbrechen
+	for(int iter=0;iter<10000;iter++){
 
 		// generate unit-vector
-		ublas::vector<double> v;
-		generate(v.begin(), v.end(), rand); 
-		v /= ublas::norm_2(v);
-		
+		ublas::vector<double> r(n);
+		generate(r.begin(), r.end(), rand); 
+		r /= ublas::norm_2(r);
+
+		ublas::vector<double> y = prod(ublas::trans(V),r);
+		sort(y.begin(),y.end());
+
+		float y_val = inner_prod(r,prod(prob.C,r));
+		if(y_val< min_y_val){
+			min_y_val = y_val;
+			min_y     = y;
+		}
 	}
-	
+
+	ublas::vector<double> x = prod(sdp_probgen.mOmega_m_1_2,min_y);
+
+	// tricky: make sure x > 0 at all times.
+	x += ublas::scalar_vector<double>(x.size(), *min_element(x.begin(),x.end()) + 1);
 
 	SeriationT ret;
+	std::vector<bool> done(x.size(),false);
+
+	// find highest component of x
+	ublas::vector<double>::iterator it = max_element(x.begin(),x.end());
+	int idx = std::distance(x.begin(),it);
+
+	for(unsigned int i=0;i<n;i++){
+		// mark as visited
+		done[idx] = false;
+
+		// make sure we do not visit again
+		*it = -1;
+
+		// [m,i] = max(x.*A(:,i));
+		ublas::vector<double> tmp = ublas::element_prod(x,ublas::column(adj,idx));
+		it = max_element(tmp.begin(),tmp.end());
+
+		int old_idx = idx;
+		idx = std::distance(tmp.begin(),it);
+
+		// point it in x, not tmp:
+		it = x.begin();
+		it += idx;
+
+		if( (done[idx] || adj(old_idx,idx)<0.5) && ret.size()<n)
+		{
+			// we reached a dead end
+			it  = max_element(x.begin(),x.end());
+			idx = std::distance(x.begin(),it);
+			L("jumping from %d to %d\n", old_idx, idx );
+		}else{
+			// remember transition
+			ret.push_back(make_pair(old_idx,idx));
+		}
+	}
+
+	for(SeriationT::iterator si=ret.begin();si!=ret.end();si++){
+		cout << "path("<<si->first<<", "<<si->second<<")."<<endl;
+	}
+
 	return ret;
 }
 
@@ -119,6 +174,5 @@ void SDPSeriationGen::setSDPWrapper(std::auto_ptr<SDPWrapper> w){
 
 SDPSeriationGen::~SDPSeriationGen()
 {
-L("Destroying SDPSeriationGen\n");
 }
 
