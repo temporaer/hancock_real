fd2a26ed1894439cd956cc5453e67f3da93bfbec
Hannes Schulz <temporaer@gmx.de>
2008-11-01 18:23:30 +0100

    add out-degree option

--- src/adjmat_gen/adjmat_gen_cfg.cpp
+++ src/adjmat_gen/adjmat_gen_cfg.cpp
@@ -16,7 +16,7 @@ AdjMatGenCfg::AdjMatGenCfg(){
 	options_description rand_amg("Random AdjMatGen");
 	rand_amg.add_options()
 		("rand_adj_mat_gen.size,n", value<int>()->default_value(20), "Size of the Matrix to be generated")
-		("rand_adj_mat_gen.prob,p", value<float>()->default_value(0.4f), "Prob of two nodes to be connected")
+		("rand_adj_mat_gen.out-degree,p", value<float>()->default_value(0.4f), "Average out-degree of a vertex")
 		("rand_adj_mat_gen.seed,s", value<float>()->default_value(1.0f), "Seed for random number generator")
 		("rand_adj_mat_gen.weighted", value<bool>()->default_value(false), "Whether edges should be weighted")
 		;
