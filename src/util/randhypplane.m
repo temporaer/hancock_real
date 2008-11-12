d8356b702c67b72c01fcc37ac526baa8a6da61cd
Hannes Schulz <temporaer@gmx.de>
2008-11-12 18:20:35 +0100

    new approach for randhypplane.m

--- src/util/randhypplane.m
+++ src/util/randhypplane.m
@@ -5,6 +5,15 @@
 values
 n = size(A,1);
 
+% functions
+Bs = [];
+function Bs = store_b (Bs,B)
+	Bs = [Bs ; B ];
+end
+function B = get_b (Bs,i,n)
+	B = Bs((i-1)*n+1:(i*n)-1, :);
+end
+
 % omega
 O = 2*eye(n); O(1) = 1; O(end) = 1;
 Op = O^0.5;
@@ -20,57 +29,56 @@ for i=1:size(A,1)
 	end
 end
 
-C = sym;
 
-res   = [];
-yvals = [];
-rv    = [];
-xs    = [];
-ys    = [];
-for i=1:1000
-	r = 2*rand(size(A,1),1)-1;
-	r = r / norm(r);
+if 1 == 1
 
-	% project v_i onto line defined by r
-	y = V'*r;
-	%y = y / norm(y);
-	y = sort(y);
-	ys = [ ys y ];
+	X  = Y;
+	Xp = Y;  % X = X + sum ... -- pertubed.
 
-	x = Op \ y;
-	yvals = [ yvals y'*y ];
-	xs    = [ xs x ];
+	for i=1:100
+		R  = chol(Xp);
+		rho = rank(Xp);
 
-	val = y'*C*y;
-	res = [ res val ];
+		% Z = in S^rho    s.t. <Z, R' Aj R> = 0    forall j=1..m. Here: Aj=eye(n)
+		Z = R' * eye(n) * R * rand(n,n);
 
-	rv  = [ rv r'*C*r ];
-	R = r*r';
-end
+		% phi(Z) = rank(Z)==rho ? 1 : -1
+		L = eig(Z);
+		if sum(diag(L)<-0.000000001) > 0
+			phiZ = -1;
+		else
+			phiZ = 1;
+		end
+		
+		ti_inv = max( phiZ * L )
+		
+		if ti_inv == 0
+			break
+		end
 
-v = V(1,:)';
+	end
 
-[max_y2,i]=max(yvals)
-[max_obj,i]=min(res)
-max_random=min(rv)
-v_1_obj=v'*C*v
 
-x = xs(:,i);
-y = ys(:,i);
+	if 0 ==  1
 
-done = zeros(n,1);
-path = [];
+		y = ys(:,i);
+		x = Op \ y;
 
-x = x + min(x) + 1;
-[m,i] = max(x);
-while(size(path) < n)
-	done(i) = 1;
-	path = [path i];
-	x(i) = -1;
-	j    = i;
-	[m,i] = max(x.*A(:,i));
-	if( (done(i) || A(i,j) <1 ) && size(path) < n)
+		done = zeros(n,1);
+		path = [];
+
+		x = x + min(x) + 1;
 		[m,i] = max(x);
-		printf('Jumping from %d to %d...\n', j,i);
+		while(size(path) < n)
+			done(i) = 1;
+			path = [path i];
+			x(i) = -1;
+			j    = i;
+			[m,i] = max(x.*A(:,i));
+			if( (done(i) || A(i,j) <1 ) && size(path) < n)
+				[m,i] = max(x);
+				printf('Jumping from %d to %d...\n', j,i);
+			end
+		end
 	end
 end
