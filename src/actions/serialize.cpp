18ec4df0c111169b7d6b09945018ed42c0023c92
Hannes Schulz <temporaer@gmx.de>
2008-11-12 20:41:17 +0100

    rename SDPSimple to Serialize

--- src/actions/serialize.cpp
+++ src/actions/serialize.cpp
@@ -6,7 +6,7 @@
 #include <configuration.hpp>
 #include <sdp_wrapper.hpp>
 #include <seriation_gen.hpp>
-#include <random_adjmat_gen.hpp>
+#include <adjmat_gen.hpp>
 
 #include "sdp_simple.hpp"
 #include <nana.h>
@@ -16,10 +16,13 @@ using namespace boost;
 
 void SDPSimple::operator()()
 {
-	dlopen("sdp_wrappers/libsdp_wrappers.so",RTLD_LAZY);
-	RandomAdjMatGen matgen;
-	matgen.configure();
-	shared_ptr<RandomAdjMatGen::AdjMatT> adjmat_ptr(matgen());
+	string adjmat_gen_name               = "RandomAdjMatGen";
+	auto_ptr<AdjMatGen> adjmat_gen       = genericFactory<AdjMatGen>::instance().create(adjmat_gen_name);
+	if(!adjmat_gen.get())
+		throw logic_error(string("Supplied AdjMatGen `") + adjmat_gen_name + "' does not exist");
+	adjmat_gen->configure();
+
+	shared_ptr<AdjMatGen::AdjMatT> adjmat_ptr((*adjmat_gen)());
 
 	string seriation_gen_name            = "SDPSeriationGen";
 	auto_ptr<SeriationGen> seriation_gen = genericFactory<SeriationGen>::instance().create(seriation_gen_name);
